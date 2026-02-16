3848bdd18fd308a588224390cac29a13613f34cc
krutiemomentileta-eng <krutiemomentileta@gmail.com>
2026-02-16 23:48:47 +0300

    1

--- server.py
+++ server.py
@@ -563,4 +563,4 @@ async def catch_all(path: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8888)
